f4118999ddb677ab017a82903eb0fef5b66b98cb
bouncingmaxt <212165011+bouncingmaxt@users.noreply.github.com>
2026-03-19 21:10:04 +0000

    Auto commit: update dependencies & generated client & code formatting

--- src/omni_monitoring/main.py
+++ src/omni_monitoring/main.py
@@ -31,6 +31,7 @@ async def http_exception_handler(request: Request, exc: HTTPException):
         content={"detail": exc.detail},
     )
 
+
 # Include routers
 app.include_router(health_router)
 app.include_router(monitoring_source_router)
