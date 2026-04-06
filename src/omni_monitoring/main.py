c05808306458a2d1074bf09dd60b70c9ebfcd63d
maxtian-xuanlan <mxt6232996077@gmail.com>
2026-04-05 21:49:18 -0700

    Add logging middleware

--- src/omni_monitoring/main.py
+++ src/omni_monitoring/main.py
@@ -7,7 +7,7 @@ from fastapi import FastAPI, HTTPException, Request
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
 from omni_python_library import init_omni_library
-from omni_python_library.middleware import AuthHeaderLoggingMiddleware
+from omni_python_library.middleware import LoggingMiddleware
 
 from omni_monitoring.routers import (
     health_router,
@@ -20,13 +20,23 @@ DEBUG = os.getenv("DEBUG", "false").lower() == "true"
 log_level = logging.DEBUG if DEBUG else logging.INFO
 logging.basicConfig(stream=sys.stdout, level=log_level, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 logger = logging.getLogger(__name__)
-logger = logging.getLogger(__name__)
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
+    logger.info("Application startup")
     init_omni_library()
+
+    loggers = ["uvicorn", "uvicorn.access", "uvicorn.error", "fastapi"]
+    for logger_name in loggers:
+        logging_logger = logging.getLogger(logger_name)
+        logging_logger.handlers = []
+        # Optional: Prevent propagation to the root logger
+        logging_logger.propagate = False
+    logger.info(f"Stopped default logging for {loggers}")
+
     yield
+    logger.info("Application shutdown")
 
 
 app = FastAPI(title="Omni Monitoring", lifespan=lifespan)
@@ -39,7 +49,7 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-app.add_middleware(AuthHeaderLoggingMiddleware)
+app.add_middleware(LoggingMiddleware)
 
 
 @app.exception_handler(HTTPException)
